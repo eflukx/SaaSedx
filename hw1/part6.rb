2b7aafdc809af8040e8727d3bfda87a6f6c9916b
eflukx <rogier@lodewijks.org>
2013-07-21 21:11:42 +0200

    Fixor

--- hw1/part6.rb
+++ hw1/part6.rb
@@ -1,5 +1,5 @@
 class Numeric
-  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  @@currencies = {'dollar' => 1.00, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
   def method_missing(method_id)
     singular_currency = method_id.to_s.gsub( /s$/, '')
     if @@currencies.has_key?(singular_currency)
@@ -8,12 +8,34 @@ class Numeric
       super
     end
   end
+  
+  def in(to_currency)
+    singular_currency = to_currency.to_s.gsub( /s$/, '')#strip trailing s
+    if @@currencies.has_key?(singular_currency)
+      self / @@currencies[singular_currency]
+    else
+      super
+    end
+  end
 end
 
 class String
-  # YOUR CODE HERE
+  def palindrome?
+    _str = self.downcase.gsub(/[^0-9a-z]/i, '')
+    if _str == _str.reverse
+      return true
+    else
+      return false
+    end
+  end
 end
 
 module Enumerable
-  # YOUR CODE HERE
+  def palindrome?
+    if self.methods.include?(:reverse)
+      self == self.reverse
+    else
+      false
+    end
+  end
 end
--- hw1/part7.rb
+++ hw1/part7.rb
@@ -1,24 +1,33 @@
 class CartesianProduct
   include Enumerable
+  def initialize(a,b)
+    @a = a
+    @b = b
+  end
+  def each
+    @a.each do |aa|
+      @b.each do |bb|
+        yield [aa,bb]
+      end
+    end
+  end
 end
 
-  def test(a, b)
-    retval = []
-    a.each{|aa|
-      b.each{|bb| retval << [aa,bb]}
-    }
-    retval
-  end
+def test(a, b)
+  retval = []
+  a.each{|aa|
+    b.each{|bb| retval << [aa,bb]}
+  }
+  retval
+end
+
+def trivial(a,b)
+  a.product(b)
+end
   
-  def test2(a, b)
-    a.each.collect{|aa| [aa,b.each.collect{|bb| bb}]}
-  end
-
-
-
+#Testspullen
 a = [:a,:b,:c]
 b = [1,2,3]
 
-puts a.product(b).inspect
-
-puts test2(a,b).inspect
+my_cp = CartesianProduct.new(a,b)
+my_cp.each{|itr| puts itr.inspect}
